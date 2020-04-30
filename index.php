636134b0edc2ecb1d459f6aee2eca7eb27fc9233
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2020-05-01 03:08:44 +0600

    Scraping anchor tag with simple dom parcer

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+include 'simple_html_dom.php';
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://www.google.com/search?q=durjoi');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+$res = curl_exec($curl);
+
+curl_close($curl);
+
+
+$html = new simple_html_dom();
+$html->load($res);
+foreach($html->find('a[href^=/url?]') as $title)
+  echo $title->plaintext . '<br>';
